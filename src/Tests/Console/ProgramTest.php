1f98dfbd83b9c98d2e81d29e2a10a33f2d802e25
ramon.puiggene <ramon.puiggene@atrapalo.com>
2017-06-23 17:01:30 +0200

    GildedRose - Initial commit

---
+++ src/Tests/Console/ProgramTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GildedRose\Tests\Console;
+
+class ProgramTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function theTruth()
+    {
+        $this->assertTrue(true);
+    }
+}
